2204f64163f0fb2c90e32c67ed58503fa0fd28e3
Ashwin <winash12@gmail.com>
2023-09-07 21:34:23 +0530

    added 2 files

---
+++ BoundingBox.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class bounding_box:
+
+    def __init__(self,points):
+        
+        self.lowerleft = points[0]
+        self.lowerright = points[1]
+        self.upperleft = points[2]
+        self.upperright = points[3]
+
+    def getUpperRightPoint(self):
+        return self.upperright
+    
+    def getUpperLeftPoint(self):
+        return self.upperleft
+
+    def getLowerRightPoint(self):
+        return self.lowerright
+
+    def getLowerLeftPoint(self):
+        return self.lowerleft
+    
+        
+class point:
+
+    def __init__(self,latitude,longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+    
+    def getLatitude(self):
+        return self.latitude
+
+    def getLongitude(self):
+        return self.longitude
+    
